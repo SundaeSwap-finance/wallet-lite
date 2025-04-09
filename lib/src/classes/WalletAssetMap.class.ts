441cd9c2e3e10bf8561d4ca1fb5befc20637ff6e
Calvin Koepke <hello@calvinkoepke.com>
2025-04-09 14:28:36 -0600

    chore: normalize ada setting and lookup

--- lib/src/classes/WalletAssetMap.class.ts
+++ lib/src/classes/WalletAssetMap.class.ts
@@ -1,5 +1,6 @@
 import { AssetAmount, IAssetAmountMetadata } from "@sundaeswap/asset";
 
+import { ADA_ASSET_ID } from "src/constants.js";
 import { normalizeAssetIdWithDot } from "../utils/assets.js";
 
 /**
@@ -15,7 +16,9 @@ export class WalletAssetMap<
    * @returns {AssetAmount<AssetMetadata> | undefined}
    */
   get(key: string): AssetAmount<AssetMetadata> | undefined {
-    return super.get(normalizeAssetIdWithDot(key));
+    return super.get(
+      this.__normalizeIfAdaAssetId(normalizeAssetIdWithDot(key)),
+    );
   }
 
   /**
@@ -26,7 +29,12 @@ export class WalletAssetMap<
    * @returns {this}
    */
   set(key: string, value: AssetAmount<AssetMetadata>): this {
-    return super.set(normalizeAssetIdWithDot(key), value);
+    return super.set(
+      this.__normalizeIfAdaAssetId(normalizeAssetIdWithDot(key)),
+      value.metadata
+        ? value.withMetadata(this.__normalizeIfAdaMetadata(value.metadata))
+        : value,
+    );
   }
 
   /**
@@ -36,6 +44,55 @@ export class WalletAssetMap<
    * @returns {AssetAmount<AssetMetadata> | undefined}
    */
   has(key: string): boolean {
-    return super.has(normalizeAssetIdWithDot(key));
+    return super.has(
+      this.__normalizeIfAdaAssetId(normalizeAssetIdWithDot(key)),
+    );
+  }
+
+  /**
+   * Overlay to enforce asset ID normalization.
+   *
+   * @param {string} key Delete the asset ID as a HEX string (either with dot or without).
+   * @returns {boolean}
+   */
+  delete(key: string): boolean {
+    return super.delete(
+      this.__normalizeIfAdaAssetId(normalizeAssetIdWithDot(key)),
+    );
+  }
+
+  /**
+   * Helper function to normalize asset ID if it is an ADA asset name.
+   *
+   * @param {string} assetId The asset ID as a HEX string (either with dot or without).
+   * @returns {string}
+   */
+  private __normalizeIfAdaAssetId(assetId: string): string {
+    const ADA_ASSET_IDS = [
+      "",
+      ".",
+      "ada.lovelace",
+      "cardano.ada",
+      "616461.6c6f76656c616365",
+    ];
+
+    if (ADA_ASSET_IDS.includes(assetId)) {
+      return ADA_ASSET_ID;
+    }
+
+    return assetId;
+  }
+
+  /**
+   * Helper function to normalize asset's metadata if it is ADA.
+   *
+   * @param {AssetMetadata} metadata The asset's metadata.
+   * @returns {AssetMetadata}
+   */
+  private __normalizeIfAdaMetadata(metadata: AssetMetadata): AssetMetadata {
+    return {
+      ...metadata,
+      assetId: this.__normalizeIfAdaAssetId(metadata.assetId),
+    };
   }
 }
---
+++ lib/src/classes/__tests__/WalletAssetMap.test.ts
@@ -0,0 +1,162 @@
+import { AssetAmount } from "@sundaeswap/asset";
+import { beforeEach, describe, expect, it } from "bun:test";
+import { ADA_ASSET_ID } from "src/constants";
+import { WalletAssetMap } from "../WalletAssetMap.class";
+
+let am: WalletAssetMap;
+
+const ADA_ASSET_IDS = [
+  "",
+  ".",
+  "ada.lovelace",
+  "cardano.ada",
+  "616461.6c6f76656c616365",
+];
+
+beforeEach(() => {
+  am = new WalletAssetMap();
+});
+
+describe("WalletAssetMap", () => {
+  describe("set()", () => {
+    it("should conform the map key to dot notation", () => {
+      expect(am.size).toEqual(0);
+
+      am.set(
+        "99b071ce8580d6a3a11b4902145adb8bfd0d2a03935af8cf66403e15524245525259",
+        new AssetAmount(100n, {
+          assetId:
+            "99b071ce8580d6a3a11b4902145adb8bfd0d2a03935af8cf66403e15.524245525259",
+          decimals: 0,
+        }),
+      );
+
+      expect(am.size).toEqual(1);
+    });
+
+    it("should conform the asset metadata to dot notation", () => {
+      expect(am.size).toEqual(0);
+
+      am.set(
+        "99b071ce8580d6a3a11b4902145adb8bfd0d2a03935af8cf66403e15.524245525259",
+        new AssetAmount(100n, {
+          assetId:
+            "99b071ce8580d6a3a11b4902145adb8bfd0d2a03935af8cf66403e15524245525259",
+          decimals: 0,
+        }),
+      );
+
+      expect(
+        am.get(
+          "99b071ce8580d6a3a11b4902145adb8bfd0d2a03935af8cf66403e15.524245525259",
+        )?.metadata.assetId,
+      ).toEqual(
+        "99b071ce8580d6a3a11b4902145adb8bfd0d2a03935af8cf66403e15.524245525259",
+      );
+    });
+
+    it("should normalize the key if using an ada assetId", () => {
+      ADA_ASSET_IDS.forEach((id) => {
+        expect(am.size).toEqual(0);
+
+        am.set(
+          id,
+          new AssetAmount(100n, {
+            assetId: id,
+            decimals: 0,
+          }),
+        );
+
+        expect(am.size).toEqual(1);
+        expect(am.get(ADA_ASSET_ID)).not.toBeUndefined();
+        expect(am.get(ADA_ASSET_ID)?.metadata.assetId).toEqual(ADA_ASSET_ID);
+        const deleted = am.delete(ADA_ASSET_ID);
+        expect(deleted).toBeTrue();
+      });
+    });
+  });
+
+  describe("get()", () => {
+    it("should normalize the key to dot notation", () => {
+      expect(am.size).toEqual(0);
+
+      am.set(
+        "99b071ce8580d6a3a11b4902145adb8bfd0d2a03935af8cf66403e15.524245525259",
+        new AssetAmount(100n, {
+          assetId:
+            "99b071ce8580d6a3a11b4902145adb8bfd0d2a03935af8cf66403e15.524245525259",
+          decimals: 0,
+        }),
+      );
+
+      expect(am.size).toEqual(1);
+
+      expect(
+        am.get(
+          "99b071ce8580d6a3a11b4902145adb8bfd0d2a03935af8cf66403e15524245525259",
+        ),
+      ).not.toBeUndefined();
+    });
+
+    it("should normalize the key if using an ada assetId", () => {
+      ADA_ASSET_IDS.forEach((id) => {
+        expect(am.size).toEqual(0);
+
+        am.set(
+          "ada.lovelace",
+          new AssetAmount(100n, {
+            assetId: "ada.lovelace",
+            decimals: 0,
+          }),
+        );
+
+        expect(am.size).toEqual(1);
+        expect(am.get(id)).not.toBeUndefined();
+        expect(am.get(id)?.metadata.assetId).toEqual(ADA_ASSET_ID);
+        const deleted = am.delete(id);
+        expect(deleted).toBeTrue();
+      });
+    });
+  });
+
+  describe("has()", () => {
+    it("should normalize the key to dot notation", () => {
+      expect(am.size).toEqual(0);
+
+      am.set(
+        "99b071ce8580d6a3a11b4902145adb8bfd0d2a03935af8cf66403e15.524245525259",
+        new AssetAmount(100n, {
+          assetId:
+            "99b071ce8580d6a3a11b4902145adb8bfd0d2a03935af8cf66403e15.524245525259",
+          decimals: 0,
+        }),
+      );
+
+      expect(am.size).toEqual(1);
+
+      expect(
+        am.has(
+          "99b071ce8580d6a3a11b4902145adb8bfd0d2a03935af8cf66403e15524245525259",
+        ),
+      ).toBeTrue();
+    });
+
+    it("should normalize the key if using an ada assetId", () => {
+      ADA_ASSET_IDS.forEach((id) => {
+        expect(am.size).toEqual(0);
+
+        am.set(
+          "ada.lovelace",
+          new AssetAmount(100n, {
+            assetId: "ada.lovelace",
+            decimals: 0,
+          }),
+        );
+
+        expect(am.size).toEqual(1);
+        expect(am.has(id)).toBeTrue();
+        am.delete(id);
+      });
+    });
+  });
+});
